d6fc69341e13d1a7449f4e58a2853196663232cf
jounsss <jouni.sahiluoto@gmail.com>
2018-08-13 06:23:49 +0300

    added chapter 3.1 variables

---
+++ c3_1_variables/src/main.rs
@@ -0,0 +1,9 @@
+const MAX_VALUE:u32 = 100_000;
+
+fn main() {
+    println!("const MAX_VALUE is {}", MAX_VALUE);
+    let mut x = 5;
+    println!("Value of x is {}", x);
+    x = 6;
+    println!("Value of x is {}", x);
+}
